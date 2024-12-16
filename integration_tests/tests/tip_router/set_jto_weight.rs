59edea0f53a7a858e9a22dfb96a934d0bf5354ce
Christian Krueger <christian@jito.wtf>
2024-12-16 16:40:55 -0700

    almost to writing tests

--- integration_tests/tests/tip_router/set_jto_weight.rs
+++ integration_tests/tests/tip_router/set_jto_weight.rs
@@ -2,6 +2,7 @@
 mod tests {
 
     use jito_tip_router_core::constants::JTO_USD_FEED;
+    use solana_sdk::account::ReadableAccount;
 
     use crate::fixtures::{test_builder::TestBuilder, TestResult};
 
@@ -9,9 +10,11 @@ mod tests {
     async fn test_switchboard_feed() -> TestResult<()> {
         let mut fixture = TestBuilder::new().await;
 
-        let test_account = fixture.get_account(&JTO_USD_FEED).await?;
+        let test_account = fixture.get_account(&JTO_USD_FEED).await?.unwrap();
 
         println!("{:?}", test_account);
+
+        println!("{:?}", test_account.data());
 
         assert!(false);
         Ok(())
