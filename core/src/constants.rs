a2b3f6c62cb2df088bb42d5c86df9d43665d17ea
Evan Batsell <ebatsell@gmail.com>
2024-12-21 19:57:16 -0500

    50

--- core/src/constants.rs
+++ core/src/constants.rs
@@ -10,9 +10,9 @@ pub const MAX_ST_MINTS: usize = 64;
 pub const MAX_VAULTS: usize = 64;
 pub const MAX_OPERATORS: usize = 256;
 pub const MIN_EPOCHS_BEFORE_STALL: u64 = 1;
-pub const MAX_EPOCHS_BEFORE_STALL: u64 = 10;
+pub const MAX_EPOCHS_BEFORE_STALL: u64 = 50;
 pub const MIN_SLOTS_AFTER_CONSENSUS: u64 = 1000;
-pub const MAX_SLOTS_AFTER_CONSENSUS: u64 = 10 * DEFAULT_SLOTS_PER_EPOCH;
+pub const MAX_SLOTS_AFTER_CONSENSUS: u64 = 50 * DEFAULT_SLOTS_PER_EPOCH;
 const PRECISE_CONSENSUS_NUMERATOR: u128 = 2;
 const PRECISE_CONSENSUS_DENOMINATOR: u128 = 3;
 pub fn precise_consensus() -> Result<PreciseNumber, TipRouterError> {
