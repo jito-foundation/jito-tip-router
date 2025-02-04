2df202c7b848ce0602df5edc4248aceae22e89ef
Evan Batsell <ebatsell@gmail.com>
2025-02-03 20:50:35 -0500

    Update claimant: staker to claimant: stake_account

--- meta_merkle_tree/src/generated_merkle_tree.rs
+++ meta_merkle_tree/src/generated_merkle_tree.rs
@@ -273,15 +273,28 @@ impl TreeNode {
                             ],
                             &TIP_DISTRIBUTION_ID,
                         );
-                        Ok(Self {
-                            claimant: delegation.staker_pubkey,
-                            claim_status_pubkey,
-                            claim_status_bump,
-                            staker_pubkey: delegation.staker_pubkey,
-                            withdrawer_pubkey: delegation.withdrawer_pubkey,
-                            amount: reward_amount,
-                            proof: None,
-                        })
+                        // Time-gated fix so slow rollout won't affect consensus
+                        if tip_router_target_epoch > 737 {
+                            Ok(Self {
+                                claimant: delegation.stake_account_pubkey,
+                                claim_status_pubkey,
+                                claim_status_bump,
+                                staker_pubkey: delegation.staker_pubkey,
+                                withdrawer_pubkey: delegation.withdrawer_pubkey,
+                                amount: reward_amount,
+                                proof: None,
+                            })
+                        } else {
+                            Ok(Self {
+                                claimant: delegation.staker_pubkey,
+                                claim_status_pubkey,
+                                claim_status_bump,
+                                staker_pubkey: delegation.staker_pubkey,
+                                withdrawer_pubkey: delegation.withdrawer_pubkey,
+                                amount: reward_amount,
+                                proof: None,
+                            })
+                        }
                     })
                     .collect::<Result<Vec<Self>, MerkleRootGeneratorError>>()?,
             );
