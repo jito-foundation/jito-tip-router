09ae852c28d84661b8986675558b67b8ac9ac083
Tommy Johnson <tommyjohnson1028@gmail.com>
2025-02-06 06:31:08 -0700

    oops..now tests compile

--- integration_tests/tests/fixtures/vault_client.rs
+++ integration_tests/tests/fixtures/vault_client.rs
@@ -6,7 +6,8 @@ use jito_restaking_core::{
     operator_vault_ticket::OperatorVaultTicket,
 };
 use jito_vault_core::{
-    config::Config, vault::Vault, vault_ncn_slasher_operator_ticket::VaultNcnSlasherOperatorTicket,
+    burn_vault::BurnVault, config::Config, vault::Vault,
+    vault_ncn_slasher_operator_ticket::VaultNcnSlasherOperatorTicket,
     vault_ncn_slasher_ticket::VaultNcnSlasherTicket, vault_ncn_ticket::VaultNcnTicket,
     vault_operator_delegation::VaultOperatorDelegation,
     vault_staker_withdrawal_ticket::VaultStakerWithdrawalTicket,
@@ -336,6 +337,7 @@ impl VaultProgramClient {
             withdrawal_fee_bps,
             reward_fee_bps,
             decimals,
+            0,
         )
         .await?;
 
@@ -662,29 +664,45 @@ impl VaultProgramClient {
         config: &Pubkey,
         vault: &Pubkey,
         vrt_mint: &Keypair,
-        token_mint: &Keypair,
+        st_mint: &Keypair,
         vault_admin: &Keypair,
         vault_base: &Keypair,
         deposit_fee_bps: u16,
         withdrawal_fee_bps: u16,
         reward_fee_bps: u16,
         decimals: u8,
+        initialize_token_amount: u64,
     ) -> Result<(), TestError> {
         let blockhash = self.banks_client.get_latest_blockhash().await?;
 
+        let admin_st_token_account =
+            get_associated_token_address(&vault_admin.pubkey(), &st_mint.pubkey());
+        let vault_st_token_account = get_associated_token_address(vault, &st_mint.pubkey());
+
+        let burn_vault =
+            BurnVault::find_program_address(&jito_vault_program::id(), &vault_base.pubkey()).0;
+
+        let burn_vault_vrt_token_account =
+            get_associated_token_address(&burn_vault, &vrt_mint.pubkey());
         self._process_transaction(&Transaction::new_signed_with_payer(
             &[initialize_vault(
                 &jito_vault_program::id(),
                 config,
                 vault,
                 &vrt_mint.pubkey(),
-                &token_mint.pubkey(),
+                &st_mint.pubkey(),
+                // TODO: Review these keys, maybe refactor to use restaking
+                &admin_st_token_account,
+                &vault_st_token_account,
+                &burn_vault,
+                &burn_vault_vrt_token_account,
                 &vault_admin.pubkey(),
                 &vault_base.pubkey(),
                 deposit_fee_bps,
                 withdrawal_fee_bps,
                 reward_fee_bps,
                 decimals,
+                initialize_token_amount,
             )],
             Some(&vault_admin.pubkey()),
             &[&vault_admin, &vrt_mint, &vault_base],
