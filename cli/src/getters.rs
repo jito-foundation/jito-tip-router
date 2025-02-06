dc09a4d8f3bf70b4d5e2d75b6ca0709cbcdf25b2
Tommy Johnson <tommyjohnson1028@gmail.com>
2025-02-05 21:37:59 -0700

    cargo build works

--- cli/src/getters.rs
+++ cli/src/getters.rs
@@ -3,7 +3,7 @@ use std::mem::size_of;
 
 use crate::handler::CliHandler;
 use anyhow::Result;
-use borsh::BorshDeserialize;
+use borsh1::BorshDeserialize;
 use jito_bytemuck::AccountDeserialize;
 use jito_restaking_core::{
     config::Config as RestakingConfig, ncn::Ncn, ncn_operator_state::NcnOperatorState,
