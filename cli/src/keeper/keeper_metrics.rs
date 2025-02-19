ef285fdceef25856aabbf27b69d6fbe55dfe4f45
Christian Krueger <christian@jito.wtf>
2025-02-19 11:19:26 -0600

    metrics

--- cli/src/keeper/keeper_metrics.rs
+++ cli/src/keeper/keeper_metrics.rs
@@ -41,9 +41,9 @@ pub async fn emit_error(title: String, error: String, message: String, keeper_ep
 
 pub async fn emit_heartbeat(tick: u64, metrics_only: bool) {
     if metrics_only {
-        datapoint_info!("tr-beta-keeper-heartbeat-operations", ("tick", tick, i64),);
-    } else {
         datapoint_info!("tr-beta-keeper-heartbeat-metrics", ("tick", tick, i64),);
+    } else {
+        datapoint_info!("tr-beta-keeper-heartbeat-operations", ("tick", tick, i64),);
     }
 }
 
