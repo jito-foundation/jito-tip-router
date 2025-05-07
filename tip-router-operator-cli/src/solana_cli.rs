b881d769ee564030328f545a47f739e2c170118c
gzalz <eric.gonzalez@jito.wtf>
2025-05-06 18:19:34 -0600

    clippy driven development

--- tip-router-operator-cli/src/solana_cli.rs
+++ tip-router-operator-cli/src/solana_cli.rs
@@ -4,8 +4,10 @@ use solana_cli::cli::CliCommand;
 use solana_cli::cli::CliConfig;
 
 pub fn catchup(rpc_url: String, our_localhost_port: u16) -> Result<String> {
-    let mut catchup_config = CliConfig::default();
-    catchup_config.json_rpc_url = rpc_url;
+    let mut catchup_config = CliConfig {
+        json_rpc_url: rpc_url,
+        ..CliConfig::default()
+    };
     catchup_config.command = CliCommand::Catchup {
         node_json_rpc_url: None,
         node_pubkey: None,
@@ -15,7 +17,7 @@ pub fn catchup(rpc_url: String, our_localhost_port: u16) -> Result<String> {
     };
     let result = process_command(&catchup_config);
     if let Err(e) = result {
-        return Err(anyhow!("Failed to execute catchup command: {}. Double check the localhost Solana RPC and the provided `localhost_port` argument.", e));
+        Err(anyhow!("Failed to execute catchup command: {}. Double check the localhost Solana RPC and the provided `localhost_port` argument.", e))
     } else {
         Ok(result.unwrap())
     }
