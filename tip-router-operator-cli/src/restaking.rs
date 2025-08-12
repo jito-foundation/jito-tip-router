34d006871350e3340098f48ef3ce386cac67244a
aoikurokawa <ht.so.ik0102@gmail.com>
2025-08-13 06:58:23 +0900

    fix: review

--- tip-router-operator-cli/src/restaking.rs
+++ tip-router-operator-cli/src/restaking.rs
@@ -61,6 +61,8 @@ impl RestakingHandler {
 
     /// Warmup NCN <> Operator state
     ///
+    /// # Process
+    ///
     /// - Check if there is an [`NcnOperatorState`] state and loudly warn (but not fail) if there is not existing
     /// - If the [`NcnOperatorState`] ticket exists and the operator is not warmed up, execute that instruction (`operator_warmup_ncn`)
     pub async fn warmup_operator(&self) -> anyhow::Result<()> {
@@ -81,7 +83,7 @@ impl RestakingHandler {
         match self.rpc_client.get_account(&ncn_operator_state_addr).await {
             Ok(account) => {
                 let ncn_operator_state = NcnOperatorState::try_from_slice_unchecked(&account.data)?;
-                if ncn_operator_state
+                if !ncn_operator_state
                     .operator_opt_in_state
                     .is_active(slot, restaking_config.epoch_length())?
                 {
@@ -104,7 +106,7 @@ impl RestakingHandler {
                     );
                     let result = self.rpc_client.send_and_confirm_transaction(&tx).await?;
 
-                    info!("Transaction confirmed: {:?}", result);
+                    info!("Transaction confirmed: {result:?}");
                 }
             }
             Err(e) => warn!("Failed to find NcnOperatorState, Please contact NCN admin!: {e}"),
@@ -114,6 +116,8 @@ impl RestakingHandler {
     }
 
     /// Create Operator <> Vault tickets
+    ///
+    /// # Process
     ///
     /// - For all vaults that have [`NcnVaultTicket`] with the NCN address, build and execute the instructions to create [`OperatorVaultTicket`] tickets, if they have not been created already.
     pub async fn create_operator_vault_tickets(&self) -> anyhow::Result<()> {
@@ -161,36 +165,45 @@ impl RestakingHandler {
             )
             .0;
 
-            if let Err(_e) = self
+            match self
                 .rpc_client
                 .get_account(&operator_vault_ticket_addr)
                 .await
             {
-                let mut ix_builder = InitializeOperatorVaultTicketBuilder::new();
-                ix_builder
-                    .config(self.restaking_config_address)
-                    .operator(self.operator_address)
-                    .vault(ncn_vault_ticket.vault)
-                    .admin(keypair.pubkey())
-                    .operator_vault_ticket(operator_vault_ticket_addr)
-                    .payer(self.keypair.pubkey());
-                let mut ix = ix_builder.instruction();
-                ix.program_id = self.restaking_program_id;
+                Ok(_account) => {
+                    info!(
+                        "OperatorVaultTicket already exists for Operator: {}, Vault: {}",
+                        self.operator_address, ncn_vault_ticket.vault
+                    );
+                    continue;
+                }
+                Err(_e) => {
+                    let mut ix_builder = InitializeOperatorVaultTicketBuilder::new();
+                    ix_builder
+                        .config(self.restaking_config_address)
+                        .operator(self.operator_address)
+                        .vault(ncn_vault_ticket.vault)
+                        .admin(keypair.pubkey())
+                        .operator_vault_ticket(operator_vault_ticket_addr)
+                        .payer(self.keypair.pubkey());
+                    let mut ix = ix_builder.instruction();
+                    ix.program_id = self.restaking_program_id;
 
-                let blockhash = self.rpc_client.get_latest_blockhash().await?;
-                let tx = Transaction::new_signed_with_payer(
-                    &[ix],
-                    Some(&self.keypair.pubkey()),
-                    &[keypair],
-                    blockhash,
-                );
-                let result = self.rpc_client.send_and_confirm_transaction(&tx).await?;
+                    let blockhash = self.rpc_client.get_latest_blockhash().await?;
+                    let tx = Transaction::new_signed_with_payer(
+                        &[ix],
+                        Some(&self.keypair.pubkey()),
+                        &[keypair],
+                        blockhash,
+                    );
+                    let result = self.rpc_client.send_and_confirm_transaction(&tx).await?;
 
-                info!("Transaction confirmed: {:?}", result);
-                info!(
-                    "Created OperatorVault Ticket, Operator: {}, Vault: {}",
-                    self.operator_address, ncn_vault_ticket.vault
-                );
+                    info!("Transaction confirmed: {:?}", result);
+                    info!(
+                        "Created OperatorVault Ticket, Operator: {}, Vault: {}",
+                        self.operator_address, ncn_vault_ticket.vault
+                    );
+                }
             }
         }
 
